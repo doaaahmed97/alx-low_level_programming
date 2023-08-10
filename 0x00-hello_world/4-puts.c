f6d22e9c36e6531d699d28b08fa29fa2c78ae4c1
doaa <doaaa_ahmed2020@outlook.com>
2023-08-10 13:14:45 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
-
-/**
- *main - this is main function
- *
- *Return: always 0
- *\
-
+#include <stdio.h>																	/**
+* main - this is main function
+*
+* Return: Always 0
+*/
 int main(void)
-{ 
-
-        puts("\"Programming is like building a multilingual puzzle");
-
-        return (0);
-}	
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
