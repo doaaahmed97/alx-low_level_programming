3ba0bae6ed86bdec787c56ba3634ebfd817734ef
doaa <doaaa_ahmed2020@outlook.com>
2023-08-11 11:30:12 -0700

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the numbers from 00 to 99
+  * main - function of alphabet
   *
-  * Return: Always (Success)
+  * Return: void
   */
 int main(void)
 {
-	int m, i;
+	int n = 97;
 
-	m = i = '0';
-
-	for (m = '0'; m <= '9'; m++)
+	while (n <= 122)
 	{
-		for (i = '0'; i <= '9'; i++)
-		{
-			putchar(m);
-			putchar(i);
-
-			if ((m != '9') || (m == '9' && i != '9'))
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+		putchar(n);
+		n++;
 	}
-
+	putchar('\n');
 	return (0);
 }
