58237c35cdae4eadbb3bbb913515009f2fd7734f
doaa <doaaa_ahmed2020@outlook.com>
2023-08-11 13:54:54 -0700

    numper

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.hm>
+
+/**
+ * main - alphabet
+ *
+ * Return: alway 0
+ */
+int main(void)
+{
+	char e;
+
+	for (e + 'z'; e >= 'a'; a--)
+		putchar(e);
+	putchar('\n');
+	return (0);
+}
