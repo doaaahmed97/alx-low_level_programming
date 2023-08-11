15b9e8380fd25298e0ce19d955ba3cae6530523c
doaa <doaaa_ahmed2020@outlook.com>
2023-08-11 14:25:21 -0700

    p

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - num9
+ *
+ * Return: alway 0
+ */
+
+int main(void)
+{
+	int t;
+
+	for (t = 0; t < 10; t++)
+	{
+		putchar(t);
+		putchar(',');
+		putchar(' ');
+	}
+		putchar('\n');
+		return (0);
+}
