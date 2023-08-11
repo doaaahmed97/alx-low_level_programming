80e22f9a62cda8c0336ee90c3e41431ac571d805
doaa <doaaa_ahmed2020@outlook.com>
2023-08-11 12:53:50 -0700

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - number
+ *
+ * Return: alway 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	printf("%d", i);
+	putchar(i);
+	putchar ('\n');
+		return (0);
+}
