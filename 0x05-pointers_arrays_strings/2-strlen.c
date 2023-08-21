ca9bbe4b2f428b2b6cbfc49876862ce103d57a99
doaa <doaaa_ahmed2020@outlook.com>
2023-08-21 12:01:26 -0700

    s

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,17 @@
 #include "main.h"
 
 /**
- *
+ *_strlen - function that returns the length of a string.
+ * @s: string to be checked
+ * Return: The length of string
+ */
+
+int _strlen(char *s)
+{
+
+int b = 0;
+
+	for (; *s++;)
+		b++;
+	return (b);
+}
