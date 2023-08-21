464079580f5602e7da1e2506a595727d431abe40
doaa <doaaa_ahmed2020@outlook.com>
2023-08-21 07:35:42 -0700

    d

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function takes a pointer to int as parameter and updates to 98
+ * @n: the value to be update
+ * Return: always 0
+*/
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+}
