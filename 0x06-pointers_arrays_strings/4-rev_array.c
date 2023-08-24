1c9985e618dced10e9f72cc9439d6277ce68a0aa
doaa <doaaa_ahmed2020@outlook.com>
2023-08-24 10:41:54 -0700

    f

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - function that reverses the content of an array of integers.
+ * @a: The array to be reversed.
+ * @n: The number of elements in the array.
+ * Return: 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int s, b, e;
+
+	b = 0;
+	e = n - 1;
+	while (b < e)
+	{
+		s = *(a + b);
+		*(a + b) = *(a + e);
+		*(a + e) = s;
+		b++;
+		e--;
+	}
+}
