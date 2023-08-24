c660004a5cdb2e9b7323a6755c6705a3388ca0c5
doaa <doaaa_ahmed2020@outlook.com>
2023-08-24 11:51:13 -0700

    d

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,19 +7,14 @@
  *@n: bytes of source
  *Return: @dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-
-	if (i < n)
-	{
-		dest[i] = '\0';
-	}
-
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i]	= src[i];
+	while (i < n)
+		dest[i++] = '\0';
 	return (dest);
 }
