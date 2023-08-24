f8dd0a9ec6d6967dc674c5114d92b2737e4f36a4
doaa <doaaa_ahmed2020@outlook.com>
2023-08-24 09:32:44 -0700

    d

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string.
+ *@dest: pointer for destination input
+ *@src: pointer for source input
+ *@n: bytes of source
+ *Return: @dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	while (*src != '\0' && n > 0)
+	{
+	*dest = *src;
+	dest++;
+	src++;
+	n--;
+	}
+	while (n > 0)
+	{
+	*dest = '\0';
+	dest++;
+	n--;
+	}
+
+	return (dest);
+}
 
