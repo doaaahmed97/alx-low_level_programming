9cd6e635331fc3b8e6d98bede61c0ba2c47af5ff
doaaahmed97 <doaa_ahmed2020@outlook.com>
2023-09-25 11:27:06 -0700

    s

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,19 +10,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *cursor;
-	listint_t **temp = head;
 
-	if (temp != NULL)
+	if (head == NULL)
+	return;
+	while (*head)
 	{
-	while (*head != NULL)
-	{
-	cursor = *head;
-	free(cursor);
-	*head = (*head)->next;
+	cursor = (*head)->next;
+	free(*head);
+	*head = cursor;
 	}
-
-	*temp = NULL;
-
-	}
-
+	*head = NULL;
 }
