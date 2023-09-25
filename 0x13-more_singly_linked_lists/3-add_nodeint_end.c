5448ad0e34e24bd06c285395c06f2eca5dae3a86
doaaahmed97 <doaa_ahmed2020@outlook.com>
2023-09-25 10:06:57 -0700

    w

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		new_node->n = n;
 		new_node->next = NULL;
 	}
-		return (NULL);
+	else
+	       	return (NULL);
 	if (cursor != NULL)
 	{
 		while (cursor->next != NULL)
